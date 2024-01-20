58300f5a5d0e245137158995e004d062298df554
ethanhob <95361850+ethanhob@users.noreply.github.com>
2024-01-20 15:11:26 +1000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,49 @@
+"use client";
 
+import PageLayout from "@/components/pageLayout";
+import Link from 'next/link';
 
 export default function Home() {
+  const openSheet = () =>{
+    window.open("https://docs.google.com/spreadsheets/d/e/2PACX-1vR2JmPFFuMGpeNUD6Biap_RSlmPR8A26YaGrUcZDD5Nh8ZZHgnKTyxnrCFcargW79oL9dpuI29_7p2y/pubhtml", '_blank')
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Welcome Team 5!
-        </p>
+    <PageLayout>
+    <main className="flex min-h-screen flex-col items-center justify-between p-16">
+      <div className="flex items-center">
+        <h1 className="text-5xl text-center text-[#606C38] font-extrabold">
+          Hello Fred!
+        </h1>
+      </div>
+
+      <div className="">
+        Select Your Tour Dates
+      </div>
+
+      <div className="flex items-center flex-col py-8 ">
+      
+        <h1 className="text-4xl text-[#606C38] font-extrabold mb-2">Step 1</h1>
+        <button className="bg-[#BC6C25] text-white font-bold py-2 px-4 rounded mb-12 text-2xl" onClick={openSheet}>
+Download CSV Template</button>     
+<h1 className="text-4xl text-[#606C38] font-bold mb-2">Step 2</h1>
+<form>
+<input type="file" title="Select your file"/>
+{/* <label htmlFor="file-input" className="mb-12 cursor-pointer bg-[#BC6C25] text-white font-bold mr-12 py-2 px-4 rounded text-2xl">
+        Select File
+      </label> */}
+<button type="submit" className="bg-[#BC6C25] cursor-pointer text-white font-bold py-2 px-4 rounded mb-12 text-2xl">
+Submit </button>
+</form>     
+</div> 
+
+
+
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">     
+
+       
       </div>
     </main>
+    </ PageLayout>
   );
-}
+};
+
