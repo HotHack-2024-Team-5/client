e7ef1e90d1f22d43ff6ee633372e9d9d8d97a746
ethanhob <95361850+ethanhob@users.noreply.github.com>
2024-01-21 08:17:05 +1000

    page router & public form access
    used router to prevent default to backend page on form submission
    changed link to vercel link for server
    
    issue with the form submission showing a response 500 error.
    Public link to spreadsheet for clients to fill out is fine.
    
    Modified styling so everything appears middle of page

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,43 @@
 "use client";
 
 import PageLayout from "@/components/pageLayout";
+import { useRouter } from "next/navigation";
 import Link from 'next/link';
+import { FormEvent, useState } from "react";
+import axios from "axios";
+
+const openSheet = () => {
+  window.open("https://docs.google.com/spreadsheets/d/1cX1I4PC73wKfjsy_ofeTpr8xq1bzlwRoZhfxSgQNEjE/edit#gid=0", '_blank')
+}
 
 export default function Home() {
-  const openSheet = () =>{
-    window.open("https://docs.google.com/spreadsheets/d/e/2PACX-1vR2JmPFFuMGpeNUD6Biap_RSlmPR8A26YaGrUcZDD5Nh8ZZHgnKTyxnrCFcargW79oL9dpuI29_7p2y/pubhtml", '_blank')
+  const [inputValue,setInputValue] = useState('');
+  const router = useRouter();
+  const onFormSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    
+
+    // @ts-ignore
+    const formData = new FormData(e.target);
+    axios.post('https://hothack-team5-server.vercel.app/csv/upload', formData);
+    console.log(formData);
+    setInputValue('');
+    // const textClear= () =>{e.target.input}
+
+    // action="http://localhost:3001/csv/upload" method="POST" encType="multipart/form-data"
   }
+
   return (
     <PageLayout>
-    <main className="flex min-h-screen flex-col items-center justify-between p-16">
+    <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="flex items-center">
-        <h1 className="text-5xl text-center text-[#606C38] font-extrabold">
+        <h1 className="text-5xl text-center text-[#606C38] font-extrabold mt-6">
           Hello Fred!
         </h1>
       </div>
 
-      <div className="">
-        Select Your Tour Dates
+      <div className="mt-6 text-2xl">
+Let us take the sting out of booking venues. Let us know your dates below.
       </div>
 
       <div className="flex items-center flex-col py-8 ">
@@ -26,19 +46,16 @@ export default function Home() {
         <button className="bg-[#BC6C25] text-white font-bold py-2 px-4 rounded mb-12 text-2xl" onClick={openSheet}>
 Download CSV Template</button>     
 <h1 className="text-4xl text-[#606C38] font-bold mb-2">Step 2</h1>
-<form action="http://localhost:3001/csv/upload" method="POST" encType="multipart/form-data">
-<input type="file" name="file" title="Select your file"/>
 
-<button type="submit" className="bg-[#BC6C25] cursor-pointer text-white font-bold py-2 px-4 rounded mb-12 text-2xl">
+<form className="flex flex-col justify-center" onSubmit={onFormSubmit}>
+<input type="text" name="name" value={inputValue} onChange={(e) => setInputValue(e.target.value)}
+/>
+<input name="file" className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file"/>
+<button type="submit" className="bg-[#BC6C25] items-center cursor-pointer text-white font-bold py-2 px-4 rounded mb-12 mt-6 text-2xl">
 Submit </button>
 </form>     
 </div> 
-
-
-
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">     
-
-       
       </div>
     </main>
     </ PageLayout>
